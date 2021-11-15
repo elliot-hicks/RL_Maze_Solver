964607c92d67a51c3a8c302c720dbdabd82e47e3
Elliot Hicks <92870956+elliot-hicks@users.noreply.github.com>
2021-11-15 15:38:43 +0000

    Delete maze-maker directory
    The name of the dir had a hyphen in it, this caused issues as the syntax failed. Major changes made to fix the bugs with importing the maze_maker package also added, this is now in the new maze_maker_package folder.

--- maze-maker/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 13 09:20:23 2021
-
-__init__ file, gives the list of modules for the __all__ var, so that we can
-do import elliots_module * (star being the main point here)
-"""
-
-__all__ = ['maze_maker']
