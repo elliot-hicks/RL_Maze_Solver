008991ebbfa8f6c39a360cfbcd51eaf1504ee82e
elliot-hicks <ppxeh1@nottingham.ac.uk>
2021-11-25 15:08:36 +0000

    added trajectory value function to value states, added loss function, updated training algorithm to update cnn every 10 episodes after exploratory period.

--- cnn_maze_solver.py
+++ cnn_maze_solver.py
@@ -22,45 +22,72 @@ import torchvision.transforms as T
 """
 TRAINING ALGORITHM PSEUDOCODE
 
+def calculate_values(trajectory_rewards, discount_factor):
+    for i in range(len(trajectory_rewards)):
+        
+        if (i == 0):
+            continue
+        else:
+            # disount the future rewards and add them to current value
+            trajectory_rewards[-i-1]  += discount_factor*trajectory_rewards[-i]
+        
+        return trajectory_rewards
+    
+def loss_fn(states, action_labels, values):
+    loss = 0
+    for i in range(len(rewards)):
+        probability_i = model(states[i])[action_labels[i]]
+        loss -= values[i]*log(probability[i])
+    return(loss)
+        
+
 def train(maze_env, model,number_episodes, discount_rate, optimiser):
     # discount rate is tha gamma in most RL formulas, used to encourage efficiency
     # exploration factor gives the proportion of episodes where the agent explores
+    agent = Agent(maze_env.maze,starting epsilon = 0.9,memory_buffer_size = 1000)
     last_episode = []
-    for episode in range(number_episodes):
+    for episode_number in range(number_of_episodes):
         steps = 0
         total_transitions = 0
         episode_done = false
         while not episode_done:
-            net_action_index, action_probabilties = argmax(model(state)), model(state)
-            agent_action_index = agent.choose_step(action)
-            action_probability = probabilities[agent_action_index] 
-            #should exploration probabilities be 1/3 or the actual prob?
-            state_before, action, state_after, reward, episode_done = agent.take_step(action)
-            reward *= (discount_rate**steps)
-            agent.memory_buffer.add([state,action,state_after,reward,probability])
+            action_probabilties = model(state)
+            action_probabilies = agent.test_actions(action_probabilities,state) # set invalid action probs to zero, renomalise
+            action, action_label = agent.choose_action(action_probabilties)
+            agent.position += action
+            agent.update_epsilon(number_of_episodes, episode_number)
+            
+            
+            #should exploration probabilities be 1/3 or the actual prob? should equal prob from NN 
+            state_before, action, state_after, reward, episode_done = maze_env.step(action)
+            agent.replay_buffer.add([state,action_label,state_after,reward])
+            
+            
+            
             steps +=1
             total_transitions+=1
-            
             if (episode_done):
-                last_episode = memory_buffer[-steps:,3]
-                total_reward = sum(memory_buffer[-steps:,3])
-                agent.memory_buffer[-steps:,3] = total_reward
-            if ((total_transitions-eploration_period)%100 == 0):
-                training_batch = agent.memory_buffer.r_sample(batch_size)
-                episode_rewards, probabilities = training_batch[3], training_bactch[4]
-                loss = loss_fn(episode_rewards, probabilities)
-                #we need an optimiser that works on final episode reward and probabilities in episode
-                optimiser.zero_grad()
-                loss.backward()
-                optimiser.step()
                 
+                last_episode = memory_buffer[-steps:,1] #list of actions in ep
                 
+                #backprop and calculate state values for trajectory
+                trajectory_values = calculate_values(memory_buffer[-steps:,3])
+                memory_buffer[-steps:,-3] = trajectory_values
+
                 
+        if ((total_transitions>eploration_period) and (episode_number % 10 == 0 )):
+            #train CNN after every 10 episodes
+            
+            training_batch = agent.memory_buffer.r_sample(batch_size)
+            states = training_batch[:,0]
+            action_labels = training_batch[:,1]
+            values = training_batch[:,2]
+            loss = loss_fn(states,action_labels,values)
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
                 
-                
-                
-            else:
-                pass
+            
     return last_episode
             
             
