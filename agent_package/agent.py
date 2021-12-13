07ad687a63e052452b7f85afe6164d29df90908b
elliot-hicks <ppxeh1@nottingham.ac.uk>
2021-12-13 13:04:41 +0000

    Style updated to conform with pep8 using black

--- agent_package/agent.py
+++ agent_package/agent.py
@@ -70,73 +70,99 @@ class ExperienceBuffer():
 
 
 class Agent:
-    def __init__(self, maze,exploration_period, starting_epsilon,buffer_size, elite_buffer_size = 600):
-        self.position = np.array([1,1])
+
+    def __init__(self, maze, exploration_period,
+                 starting_epsilon,
+                 buffer_size,
+                 elite_buffer_size=600):
+        self.position = np.array([1, 1])
         self.environment = maze
         self.exploration_period = exploration_period
         self.epsilon = starting_epsilon
-        self.epsilon_lower_bound = starting_epsilon/100
+        self.epsilon_lower_bound = starting_epsilon / 100
         self.replay_buffer = ExperienceBuffer(buffer_size)
         self.elite_experience_buffer = ExperienceBuffer(elite_buffer_size)
         self.elite_steps = []
         # we use deques for more efficient appends and size capping
-        self.action_space = np.array([[-1,0],[0,1],[1,0],[0,-1]])
-        self.action_space_labels = np.array([0,1,2,3])
-        
-    def test_actions(self,action_probabilities):
-        #remove possibility to step in to wall
+        self.action_space = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])
+        self.action_space_labels = np.array([0, 1, 2, 3])
+
+    def test_actions(self, action_probabilities):
+        """
+        Removed possibility of agent stepping in to walls, speeds up training
+        as the CNN has one less thing to learn and the rewards function
+        can be made simply, with no tuning of rewards for wall steps needed.
+        """
         for action in range(4):
-            test_position = self.position + self.action_space[action,:]
-            #test if new position is a wall (val = -1)
-            if (self.environment[test_position[0],test_position[1]] == -1):
-                action_probabilities[action] = 0   
-        #renormalise
-        normed_probabilities = normalise(action_probabilities)
-        return (normed_probabilities)              
-    
+            test_position = self.position + self.action_space[action, :]
+            # Test if new position is a wall (val = -1):
+            if (self.environment[test_position[0], test_position[1]] == -1):
+                action_probabilities[action] = 0
+        normed_probabilities = normalise(action_probabilities)  # Renormalize
+        return (normed_probabilities)
+
     def choose_action(self, probabilities):
-        # implement epsilon-greedy,explotration prob of self.epsilon
-        policy_action_label = np.random.choice(self.action_space_labels, p = probabilities)
+        """
+        Parameters
+        ----------
+        probabilities : NumPy array (floats)
+            Porbabilties perscribed by policy function corresponding to the
+            agent actions. Implemented after action testing, exmploys
+            epsilon-greedy approach.
+        Returns
+        -------
+        random_(policy_)action: action chosen by exploration(explotation)
+        random_(policy_)action_label: encoding corresponding to action:
+            0,1,2,3 = up,right,down,left
+
+        """
+        # Get policy action:
+        policy_action_label = np.random.choice(self.action_space_labels,
+                                               p=probabilities)
         policy_action = self.action_space[policy_action_label]
-        if np.random.uniform(0,1)<self.epsilon:
-            probabilities[policy_action_label]=0 # agent must choose alt step
-            probabilities = normalise(probabilities)
+        if np.random.uniform(0, 1) < self.epsilon:
+            probabilities[policy_action_label] = 0  # Policy not followed
+            probabilities = normalise(probabilities)  # Renormalize
             try:
-                random_action_label = np.random.choice(self.action_space_labels, p = probabilities)
+                random_action_label = np.random.choice(self.action_space_labels,
+                                                       p=probabilities)
                 random_action = self.action_space[random_action_label]
                 return random_action, random_action_label
             except ValueError:
-                # probabilities are NANs if all zeros after removing policy's choice
-                # this happens when only one action is valid
+                """
+                If only one action valid, the action space will be the policy
+                action, after removing policy action in epslion greedy step,
+                all the probabilties would be zero, causing a 0 division error.
+                """
                 return policy_action, policy_action_label
-        else:    
+        else:
             return policy_action, policy_action_label
-    
-    def update_epsilon(self,episode, number_of_episodes):
-        #epsilon should start high and then start to decrease 
+
+    def update_epsilon(self, episode, number_of_episodes):
+        # Epsilon should stay at 1 for exploration period:
         if (episode < self.exploration_period):
             pass
-            # keep epsilon constant for first 10% of runs to encourage exploration early on
+        # Epsilon should not go below lower bound of 1%
         elif (self.epsilon < self.epsilon_lower_bound):
             self.epsilon = self.epsilon_lower_bound
-        elif ((episode-self.exploration_period) % 50 == 0):
+        # Decrease epsilon by 10% every 10 eps past exploration period:
+        elif ((episode-self.exploration_period) % 10 == 0):
             print("epsilon reduced")
-            self.epsilon *= 0.9#(number_of_episodes-episode)/number_of_episodes
-            # reduce epsilon by 5% every 100 episodes
-    
-    def update_elite_buffer(self,n_steps):
-        #add the last n steps (n = steps), to the elite buffer from the memory buffer
+            self.epsilon *= 0.9
+
+    def update_elite_buffer(self, n_steps):
+        # Add the last n steps (n = steps), to the elite buffer
         if self.elite_experience_buffer.is_full():
-            if n_steps<max(self.elite_steps):
+            if n_steps < max(self.elite_steps):
                 add_new_elite_memory = True
             else:
                 add_new_elite_memory = False
         else:
             add_new_elite_memory = True
-        
+
         if add_new_elite_memory:
             print("Elite memory found")
             self.elite_steps.append(n_steps)
-            for i in range(1,n_steps+1):
-                self.elite_experience_buffer.add(self.replay_buffer.memory_buffer[-i,:])
-            
+            for i in range(1, n_steps + 1):
+                self.elite_experience_buffer.add(
+                    self.replay_buffer.memory_buffer[-i, :])
